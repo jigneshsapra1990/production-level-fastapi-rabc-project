a1e65c90f00ab1d61f01eac2a34739186564d3c1
jigneshsapra1990 <jignesh.sapra@gmail.com>
2026-05-25 19:09:02 +0530

    add BaseModel class with UUID primary key and timestamp fields

---
+++ app/models/base.py
@@ -0,0 +1,12 @@
+import uuid
+from sqlalchemy import Column, String,DateTime, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from app.db.database import Base
+
+class BaseModel(Base):
+    __abstract__ = True
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    is_active = Column(Boolean, default=True)
